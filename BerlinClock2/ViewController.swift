77659086b5418626a4308c2c51c5dc8348754ae6
aknur <95418973+aknurkappar@users.noreply.github.com>
2023-09-30 00:51:01 +0600

    Initial Commit

---
+++ BerlinClock2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BerlinClock2
+//
+//  Created by коня on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
